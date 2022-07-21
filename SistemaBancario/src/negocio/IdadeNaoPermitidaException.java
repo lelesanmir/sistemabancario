b186f6f6319c924ec6e06fd921d23fe095b742f0
Leonardo dos Santos Miranda <91301972+lelesanmir@users.noreply.github.com>
2022-07-21 16:10:02 -0300

    Sistema bancário Teste automatizados com Junit.
    Diversos testes como fazer conta, add e remover cliente, transferência e mais alguns testes.

---
+++ SistemaBancario/src/negocio/IdadeNaoPermitidaException.java
@@ -0,0 +1,17 @@
+package negocio;
+
+/**
+ * Exceção a ser lançada quando a idade de um possível novo cliente não for aceita.
+ * 
+ * @author Gustavo Farias
+ */
+@SuppressWarnings("serial")
+public class IdadeNaoPermitidaException extends Exception {
+
+	public static String MSG_IDADE_INVALIDA = "A idade do cliente precisa estar entre 18 e 65 anos.";
+	
+	public IdadeNaoPermitidaException(String msg) {
+		super(msg);
+	}
+
+}
